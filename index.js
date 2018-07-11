bdccd9b36b8e7996162c35362e093a1ac02b0aa7
Shin JinHo <dpdlalslr@gmail.com>
2018-07-11 19:21:03 +0900

    feat: Add framework files
    added index.htm index.js
    added license
    added basic images

---
+++ index.js
@@ -0,0 +1,52 @@
+﻿loaded=1;
+function loadContent(){
+var s='';
+
+//****************************************************************************************************
+
+s+=wH("Bruno Mars","Marry You");
+s+=wA("It's a beautiful night, we're looking for something dumb to do");
+s+=wA("Hey baby, I think I wanna marry you");
+s+=wA("Is it the look in your eyes, or is it this dancing juice");
+s+=wA("Who cares baby, I think I wanna marry you");
+s+=wF();
+
+//****************************************************************************************************
+
+s+=wH("Coldplay","In My Place");
+s+=wA("In my place, in my place");
+s+=wA("Were lines that I couldn't change");
+s+=wA("I was lost, oh yeah");
+s+=wA("I was lost, I was lost");
+s+=wA("Crossed lines I shouldn't have crossed");
+s+=wF();
+
+//****************************************************************************************************
+
+s+=wH("Bruno Mars","Marry You");
+s+=wA("It's a beautiful night, we're looking for something dumb to do");
+s+=wA("Hey baby, I think I wanna marry you");
+s+=wA("Is it the look in your eyes, or is it this dancing juice");
+s+=wA("Who cares baby, I think I wanna marry you");
+s+=wF();
+
+//****************************************************************************************************
+
+s+=wH("Coldplay","In My Place");
+s+=wA("In my place, in my place");
+s+=wA("Were lines that I couldn't change");
+s+=wA("I was lost, oh yeah");
+s+=wA("I was lost, I was lost");
+s+=wA("Crossed lines I shouldn't have crossed");
+s+=wF();
+
+//****************************************************************************************************
+
+wForm(s);
+
+wM(0,"Home","test1.png","7px 7px","26px 26px");
+wM(1,"Book","test2.png","7px 7px","26px 26px");
+wM(2,"Star","test3.png","10px 10px","20px 20px");
+wM(3,"Flower","test4.png","7px 7px","26px 26px");
+
+}
