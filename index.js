74fcaa4184f750020e1ca89d2e991346dea79dca
Shin JinHo <dpdlalslr@gmail.com>
2018-07-14 11:27:55 +0900

    feat: Increase max number of tabs
    increased number of tabs; 4 -> infinity
    both pc version and mobile version

--- index.js
+++ index.js
@@ -32,6 +32,15 @@ s+=wF();
 
 //****************************************************************************************************
 
+s+=wH("Bruno Mars","Marry You");
+s+=wA("It's a beautiful night, we're looking for something dumb to do");
+s+=wA("Hey baby, I think I wanna marry you");
+s+=wA("Is it the look in your eyes, or is it this dancing juice");
+s+=wA("Who cares baby, I think I wanna marry you");
+s+=wF();
+
+//****************************************************************************************************
+
 s+=wH("Coldplay","In My Place");
 s+=wA("In my place, in my place");
 s+=wA("Were lines that I couldn't change");
@@ -44,9 +53,11 @@ s+=wF();
 
 wForm(s);
 
+wBack("#index","test1.png","7px 7px","26px 26px");
 wM(0,"Home","test1.png","7px 7px","26px 26px");
 wM(1,"Book","test2.png","7px 7px","26px 26px");
 wM(2,"Star","test3.png","10px 10px","20px 20px");
 wM(3,"Flower","test4.png","7px 7px","26px 26px");
+wM(4,"Flower","test4.png","7px 7px","26px 26px");
 
 }
